e7919200e0e0abd6d8492d75d137422dd9b96793
kalpana11gurnani <gurnanikalpana11@gmail.com>
2026-05-27 11:04:04 +0530

    updated advance java assignments

---
+++ Advance Java/EcommerceProdCart/src/main/java/com/demo/dao/LoginDaoImpl.java
@@ -0,0 +1,39 @@
+package com.demo.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.demo.model.MyUser;
+
+public class LoginDaoImpl implements LoginDao {
+	static Connection conn;
+	static
+	{
+		conn=DBUtil.getMyConnection();
+	}
+
+	@Override
+	public MyUser authenticateUser(String uname, String pswd) {
+		try {
+			PreparedStatement pst=conn.prepareStatement("select * from myuser where username=? and password=?");
+			pst.setString(1,uname);
+			pst.setString(2, pswd);
+			ResultSet rs=pst.executeQuery();
+			if(rs.next())
+			{
+				MyUser u=new MyUser(rs.getString(1),rs.getString(2),rs.getString(3));
+				return u;
+						
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
